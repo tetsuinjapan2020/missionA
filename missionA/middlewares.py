e6510b4e6b408ac5c4d669d6ebbe85f7f981d25b
tetsuinjapan2020 <tetsuinjapan2020@gmail.com>
2021-03-04 07:02:07 +0900

    Format and Rename

--- missionA/middlewares.py
+++ missionA/middlewares.py
@@ -3,8 +3,9 @@ from django.utils.deprecation import MiddlewareMixin
 from django.http.response import HttpResponse
 from django.core.cache import caches
 
-MAX_REQUEST_PER_MINIUTE = 60
+MAX_REQUEST_PER_MINUTE = 60
 PERIOD = 60
+
 
 class LimitRequestMiddleware(MiddlewareMixin):
 
@@ -12,10 +13,10 @@ class LimitRequestMiddleware(MiddlewareMixin):
         ip = request.META.get("REMOTE_ADDR")
         now = time.time()
         cache = caches['default']
-        requests = cache.get(ip,[])
-        while requests and time.time() -  requests[-1] > PERIOD:
+        requests = cache.get(ip, [])
+        while requests and time.time() - requests[-1] > PERIOD:
             requests.pop()
         requests.insert(0, time.time())
         cache.set(ip, requests, timeout=PERIOD)
-        if len(requests) > MAX_REQUEST_PER_MINIUTE:
-            return HttpResponse("Too often")
+        if len(requests) > MAX_REQUEST_PER_MINUTE:
+            return HttpResponse("Too many requests!!!")
